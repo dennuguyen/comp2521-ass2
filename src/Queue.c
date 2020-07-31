c9aa97e9c935c827a62a6d786e326f40829450b5
z5209611 <z5209611@cse.unsw.edu.au>
2020-07-31 21:26:20 +1000

    Fixing the shortest path function

--- src/Queue.c
+++ src/Queue.c
@@ -83,7 +83,7 @@ void Enqueue(QueueView q, PlaceId location, int level)
     if (q == NULL || q->head == NULL)
         return;
 
-    TrailNode head = NewNode(location, level);
+    QueueNode head = NewNode(location, level);
 
     if (q->head == NULL)
         q->head = head;
@@ -100,8 +100,8 @@ QueueNode Dequeue(QueueView q)
     if (q == NULL || q->head == NULL)
         return NULL;
 
-    TrailNode temp = q->head;
-    TrailNode node = &(trailNode){.location = temp->location, .isVampire = temp->isVampire};
+    QueueNode temp = q->head;
+    QueueNode node = &(queueNode){.location = temp->location, .level = temp->level};
     q->head = q->head->next;
 
     if (q->head == NULL)
