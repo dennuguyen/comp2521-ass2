ccc3e4c0f2ecea16d6cfa7d2d721dcaa23896c71
Vi <z5205798@unsw.edu.au>
2020-08-01 21:03:17 +1000

    Add minimum moves condition

--- src/DraculaView.c
+++ src/DraculaView.c
@@ -92,15 +92,15 @@ PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves)
 	{
 		if (lastFiveLocations[i] == HIDE)
 			canHide = false;
-		else if (lastFiveLocations[i] == DOUBLE_BACK_1)
+		if (lastFiveLocations[i] == DOUBLE_BACK_1 || numDraculaMoves < 1)
 			canDoubleBack1 = false;
-		else if (lastFiveLocations[i] == DOUBLE_BACK_2)
+		if (lastFiveLocations[i] == DOUBLE_BACK_2 || numDraculaMoves < 2) 
 			canDoubleBack2 = false;
-		else if (lastFiveLocations[i] == DOUBLE_BACK_3)
+		if (lastFiveLocations[i] == DOUBLE_BACK_3 || numDraculaMoves < 3)
 			canDoubleBack3 = false;
-		else if (lastFiveLocations[i] == DOUBLE_BACK_4)
+		if (lastFiveLocations[i] == DOUBLE_BACK_4 || numDraculaMoves < 4)
 			canDoubleBack4 = false;
-		else if (lastFiveLocations[i] == DOUBLE_BACK_5)
+		if (lastFiveLocations[i] == DOUBLE_BACK_5 || numDraculaMoves < 5)
 			canDoubleBack5 = false;
 	}
 
